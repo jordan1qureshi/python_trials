bee3c30a5d985ff6e61155ab38b6ded92a4187e1
jordan1qureshi <jordan.schreiner-qureshi@stu.ocsb.ca>
2024-12-10 17:33:47 +0000

    fewferferfe

--- trial.py
+++ trial.py
@@ -1,1 +1,33 @@
+int(input("Please choose a pizza size (small, medium, large): 
+")
 
+if pizza size = small
+print("Pizza cost 8$")
+
+if pizza size = medium
+print("Pizza cost 10$")
+
+if pizza size = large
+print("Pizza cost 12$")
+
+output("would you like to order?")
+
+if yes = yes
+print("yes")
+
+if no = noprint("no")
+
+if=yes
+print("what size would u like to order")
+
+if pizza size = small
+print("Pizza cost 8$")
+
+if pizza size = medium
+print("Pizza cost 10$")
+
+if pizza size = large
+print("Pizza cost 12$")
+
+if order finished
+print("your order has been complete pleaase wait for your food now.")
